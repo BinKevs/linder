2b33c4037e18ba9c2ce744a9a75e6ab3e7dba2c5
Kevin Bryan Buenaseda <50974105+BinKevs@users.noreply.github.com>
2023-02-23 14:16:10 +0000

    For final toucher

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -12,9 +12,9 @@ function Landing() {
   return (
     <>
       <div className="bg-[#46B0C8]">
-        <div className="flex md:flex-row flex-col md:h-screen relative md:w-[80%] mx-auto justify-center border-2">
-          <div className="md:w-[50%]  md:mx-0 mx-auto md:h-[100%] h-screen flex text-white ">
-            <div className="m-auto md:w-[80%] space-y-8 text-center font-RobotoSlab ">
+        <div className="flex md:flex-row flex-col md:h-screen relative md:w-[80%] mx-auto justify-center">
+          <div className="md:w-[50%]  md:mx-0 mx-auto md:h-[100%] h-[80vh] mt-[20vh] flex text-white">
+            <div className="m-auto md:w-[80%] w-[90%] space-y-8 text-center font-RobotoSlab ">
               <div className="md:text-7xl text-4xl font-bold ">
                 The Dog That Couldn't{" "}
                 <span className="text-[#AC3318]">Bark</span>
@@ -29,16 +29,16 @@ function Landing() {
               </div>
             </div>
           </div>{" "}
-          {/* <div className="md:w-[50%] w-[80%] flex h-screen ">
-            <div className=" m-auto flex flex-col justify-center items-center">
-              <div className="md:flex-row flex-col min-w-[320px] shadow-xl rounded-lg flex space-x-8">
+          <div className="md:w-[50%] w-[80%] mx-auto flex md:h-screen justify-center items-center  ">
+            <div className=" m-auto flex flex-col justify-center items-center w-full">
+              <div className="md:flex-row flex-col min-w-[320px] flex md:space-x-8 md:space-y-0 space-y-8 justify-center ">
                 <img
-                  className="md:w-[400px]"
+                  className="md:w-[400px] md:min-w-[200px] w-[200px] mx-auto"
                   src={dogThatCouldNotBarkFrontGreen}
                   alt=""
                 />
                 <img
-                  className="md:w-[400px]"
+                  className="md:w-[400px] md:min-w-[200px] w-[200px] mx-auto"
                   src={dogThatCouldNotBarkFrontBlue}
                   alt=""
                 />
@@ -47,15 +47,15 @@ function Landing() {
               <a
                 target="_blank"
                 href="https://www.amazon.com/Dog-That-Couldnt-Bark-ebook/dp/B0BPJYXGXQ/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1675090646&sr=8-1"
-                class="uppercase font-bold text-white bg-[#AC3318] border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg my-8 max-w-[50%] md:min-w-[320px] w-full text-center z-[9999]"
+                class="uppercase font-bold text-white bg-[#AC3318] border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded md:text-lg my-8 max-w-[50%] md:min-w-[320px] w-full text-center z-[9999]"
                 rel="noreferrer"
               >
                 Buy on Amazon
               </a>
             </div>
-          </div> */}
+          </div>
         </div>
-        {/* <div class="custom-shape-divider-bottom-1675515602">
+        <div class="custom-shape-divider-bottom-1675515602 md:block hidden">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -67,23 +67,23 @@ function Landing() {
               class="shape-fill"
             ></path>
           </svg>
-        </div> */}
+        </div>
       </div>
-      {/* <div className="h-full bg-white flex text-white">
+      <div className="h-full bg-white flex text-white">
         <div>
-          <div className="h-screen flex">
-            <div className="w-[70%] m-auto text-center flex space-x-16 ">
-              <div className="bg-[#46B0C8] rounded-bl-[780px] min-w-[600px]">
+          <div className="md:h-screen flex">
+            <div className="w-[70%] m-auto md:text-center flex md:flex-row flex-col md:space-x-16 md:space-y-0 space-y-8 ">
+              <div className="bg-[#46B0C8] rounded-bl-[780px] md:min-w-[600px] min-w-[250px]">
                 <img
-                  className="mx-auto rounded-3xl min-w-[350px]"
+                  className="mx-auto rounded-3xl md:min-w-[350px] min-w-[150px]"
                   src={linderWtOutBg}
                   alt=""
                 />
               </div>
 
-              <div className=" md:text-2xl text-xl text-black my-auto text-justify">
+              <div className=" md:text-2xl text-md text-black my-auto md:text-justify">
                 Hello, my name is{" "}
-                <span className="font-bold text-7xl text-[#46B0C8] p-8">
+                <span className="font-bold md:text-7xl text-2xl text-[#46B0C8] p-8">
                   {" "}
                   Jeralynne Linder{" "}
                 </span>
@@ -99,7 +99,7 @@ function Landing() {
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
 
       {/* <div className="bg-white md:h-screen flex pt-">
         <div className="m-auto w-[100%] ">
